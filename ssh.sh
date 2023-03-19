9e1435f26d30ef39f4922fe8029bd01f6b8a96b1
Letu Wang <wangletu57@gmail.com>
2023-03-18 21:20:49 -0700

    update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -7,7 +7,7 @@ ssh-keygen -t ed25519 -C wangletu57@gmail.com -f ~/.ssh/id_ed25519
 
 # Adding your SSH key to the ssh-agent
 bass eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
+/usr/bin/ssh-add --apple-use-keychain ~/.ssh/id_ed25519 # use macos pre-installed version
 
 # Adding your SSH key to your GitHub account
 echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub"
