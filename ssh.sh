d39e1e78b7a7e32cd0150617a5c25f9aa701f501
Letu Wang <wangletu57@gmail.com>
2023-03-18 18:17:19 -0700

    fix ssh-add in ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -9,7 +9,7 @@ ssh-keygen -t ed25519 -C wangletu57@gmail.com -f ~/.ssh/id_ed25519
 # Adding your SSH key to the ssh-agent
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
 bass eval "$(ssh-agent -s)"
-ssh-add -K ~/.ssh/id_ed25519
+ssh-add ~/.ssh/id_ed25519
 
 # Adding your SSH key to your GitHub account
 # https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
