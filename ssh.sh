20a4800168339f8f22701d104ce5b74fa63af8db
Letu Wang <wangletu57@gmail.com>
2023-03-18 19:40:26 -0700

    simplify ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -3,14 +3,11 @@
 echo "Generating a new SSH key for GitHub..."
 
 # Generating a new SSH key
-# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
 ssh-keygen -t ed25519 -C wangletu57@gmail.com -f ~/.ssh/id_ed25519
 
 # Adding your SSH key to the ssh-agent
-# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
 bass eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
 # Adding your SSH key to your GitHub account
-# https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
 echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub"
